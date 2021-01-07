7a6afb9d3f902b2d49ff9f9f0ed873f9ceffd049
Julia Fransiska L <jflumbansiantar@gmail.com>
2021-01-08 03:17:02 +0700

    oop study

---
+++ oop.js
@@ -0,0 +1,194 @@
+//OOP STUDY
+/**
+ * OOP stands for Object Oriented Program and has four pillars (Inheritance, Encapsulation, Abstraction and Polymorphism).
+ * First, let's make a class that contains something.
+*/
+class Person {  //This class is called Person AND THIS IS THE PARENT
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    introduce() { //mix it with tenary and its pawsome!!
+        console.log(`Hi, my name is ${this.name} (${this.age}, ${this.gender})`)
+    }
+}
+
+//Let say we have three objects of people
+let person1 = ('June', 34,'Female')
+
+let person2 = {
+    name: 'Arunika',
+    age: 8,
+    gender: 'Female'
+}
+
+let person3 = {
+    name: 'Yoe',
+    age: 26,
+    gender: 'Male'
+}
+
+/**Inheritance
+ * IS basically just copying from the parent class to the child class.
+ * Cause we already have PARENT, we just need to make the child class to inherit the parent.
+*/
+class Adult extends Person {
+    constructor(name, age, gender, isAdult) {
+        super(name, age, gender) //call parent constructor
+        this.isAdult = (this.age >= 20)
+
+    }
+    introduce() { //mix it with tenary and its pawsome!!
+        console.log(`Hi, my name is ${this.name} (${this.age}, ${this.gender}) and I am ${this.isAdult ? ' an Adult' : ' a Child'}`)
+    }
+}
+
+let adult1 = new Adult('Anika', 91, 'Female');
+// adult1.introduce()
+
+/**Encapsulation
+ * We can hide some shades here :) ((if u know what I mean)). Anyway, we can choose some property to hide.
+ * There is something called VISIBILITY and it has three methods
+ * 1. PUBLIC
+ * This means all the properties are accessible just like the one we did in INHERITANCE. No shade and no secret.
+ * 2. PROTECTED
+ * 
+ * 3. PRIVATE
+ * THis means the properties can't be accessed outside the declaration
+*/
+//PRIVATE
+class Human {
+
+    // static isTalk = true;
+
+    constructor(name, age, gender, isTalk) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.isTalk = true;
+    }
+
+    // #doGossip = true;
+
+}
+
+class Adults extends Human {
+    constructor(name, age, gender, isTalk, isAdult) {
+        super(name, age, gender, isTalk); //call parent constructor
+        this.isAdult = (this.age >= 20)
+
+    }
+    introduce() { //mix it with tenary and its pawsome!!
+        console.log(`Hi, my name is ${this.name} (${this.age}, ${this.gender}).
+        I am ${this.isAdult ? ' an Adult' : ' a Child'}.
+        I am ${this.isTalk ? ' talk' : 'lip shut'}`
+        )
+    }
+}
+
+let adult2 = new Adults('Anika', 91, 'Female');
+// adult2.talk();
+
+/**Abstraction
+ * This is the generalisation of an idea and its abstract(?)#notquiteundertandwhatitsmean
+ * 
+ */
+class Jobseeker extends Person {
+    constructor(name, age, gender, isAdult, status) {
+        super(name, age, gender)
+        this.isAdult = (this.age >= 20)
+        this.status = this.isAdult;
+    }
+
+    introduce() {
+        console.log(
+            `Hi, my name is ${this.name}(${this.age}, ${this.gender}) and I am ${this.isAdult ? ' an Adult' : ' a Child'}
+            I am ${this.status ? 'an Employee' : 'a Jobseeker'}
+            `
+        )
+    }
+
+    proffesion() {
+        if (this.status == true) {
+            console.log('I am a programmer');
+        } else {
+            console.log('I am trying to get a job.')
+        }
+        
+    }
+}
+
+let adult3 = new Jobseeker('Andika Kangen Band', 29, 'Male')
+// adult3.introduce('--ini introduce')
+// adult3.proffesion('--ini proffesion')
+
+/**Polymorphism
+ * This class is a class that could have a lot of form of its child class. A bit different from its parent class.
+ * Something called mix-ins used in this pillar.
+*/
+//lets make four type of jobs
+const PublicServer = Base => class extends Base {
+    save() {
+        console.log('Thanks for saving me.');
+    }
+}
+
+const Military = Base => class extends Base {
+    shoot() {
+        console.log('DOR!!');
+    }
+    agent() {
+        console.log('I am here to get information.')
+    }
+}
+
+const School = Base => class extends Base {
+    teach() {
+        console.log('Dont forget your homework and stay safe.');
+    }
+}
+
+const Music = Base => class extends Base {
+    got7() {
+        console.log('Come and get it! GOT7!');
+    }
+}
+
+//then wwe make the child class
+
+class Doctor extends PublicServer(Military(Person)) {
+
+    // static workplace = 'Hospital';
+
+    constructor(name, age, gender) {
+        super(name, age, gender)
+    }
+    work() {
+        super.introduce()
+        super.shoot()
+        super.save()
+    }
+}
+
+class Spy extends Music(Military(Person)) {
+
+    // static workplace = 'Hospital';
+
+    constructor(name, age, gender) {
+        super(name, age, gender)
+    }
+    work() {
+        super.got7()
+        super.introduce()
+        super.agent()
+        // super.save()
+    }
+}
+
+let adult4 = new Doctor('Karin', 29, 'Female')
+// adult4.work()
+
+let adult5 = new Spy('Jack', 27, 'Male')
+adult5.work()
--- test4.js
+++ test4.js
@@ -11,13 +11,19 @@
 
 const prime = (n) => {
     //Make a temporary variable to count primes
+    console.time('Start')
     let temp = 0;
-    //Loop the n number
+    let primes = []
+    let xi = []
+    let xj = []
+    //Loop the n number. 
     for (let i = 1; i <= n; i++) {
         //Declare another temporary variable
         let number = 0;
+        xi.push(i);
         //Loop the i to compare with j
         for (let j = 2; j < i; j++) {
+            xj.push(j)
             if (i % j === 0) {
                 number = 1;
                 break
@@ -26,13 +32,18 @@ const prime = (n) => {
         //Check if i is a prime or not 
         if (i > 1 && number === 0) {
             temp++;
+            primes.push(i);
         }
     }
     console.log(temp)
+    console.timeEnd('Start')
+    // console.log(primes, '--primes');
+    // console.log(xi, '--i')
+    // console.log(xj, '--j')
 }
 
-// prime(10)
-// prime(1000)
+prime(10)
+prime(1000)
 
 
 /**
@@ -62,19 +73,26 @@ const prime = (n) => {
 
 //Take input from the user 
 const palindrome = (str) => {
+    // console.time('start')
     //A temporary array to put letters from strings
-    let tempString = new Array(str.length);
+    let tempString = new Array(str.length); //[0][1][2]....
     for (let x = 0; x < tempString.length; x++) {
         tempString[x] = new Array(str.length);
     }
+    // console.log(tempString, '-line 79');
 
     //Letters in the String
+    // let i = 0;
     let maxLength = 1;
-    let i = 0;
     let start = 0;
 
     //check if letter is 1
-    while (i < str.length) {
+    // while (i < str.length) {
+    //     tempString[i][i] = true;
+    //     i = i + 1;
+    // console.log(tempString, '--line 90')
+    // }
+    for (let i = 0; i < str.length; i++) {
         tempString[i][i] = true;
         i = i + 1;
     }
@@ -88,7 +106,16 @@ const palindrome = (str) => {
             maxLength = 2;
         }
         i = i + 1;
+        // console.log(tempString, '--line 102')
     }
+    // for (let i = 0; i <(str.length - 1); i++) {
+    //     if (str[i] === str[i + 1]) {
+    //         tempString[i][i + 1] = true;
+    //         start = i;
+    //         maxLength = 2;
+    //     }
+    //     i = i + 1;
+    // }
 
     //Check if the letters of the string are greater than 2
     let k = 3;
@@ -96,6 +123,7 @@ const palindrome = (str) => {
         i = 0;
         while (i < (str.length - k + 1)) {
             j = i + k - 1;
+            console.log(i, j, '-line 110');
 
             if (tempString[i + 1][j - 1] && str[i] == str[j]) {
                 tempString[i][j] = true;
@@ -108,20 +136,41 @@ const palindrome = (str) => {
             i = i + 1;
         }
         k = k + 1;
+        console.log(tempString, '--line 123')
     }
+
+    // for (let k = 3; k <= str.length; k++) { //n(1)
+    //     for (let i = 0; i < (str.length - k + 1); i++) {   //n(2)
+    //         j = i + k - 1;
+    //         console.log(i, j, '-line 110');
+
+    //         if (tempString[i + 1][j - 1] && str[i] == str[j]) {
+    //             tempString[i][j] = true;
+
+    //             if (k > maxLength) {
+    //                 start = i;
+    //                 maxLength = k;
+    //             }
+    //         }
+    //         i = i + 1;
+    //     }
+    //     k = k + 1
+    // }
+
     //Print the palindrome letters in strings
     function testPalindrome(str, low, high) {
         let newStr = "";
         for (let i = low; i <= high; i++) {
             newStr += str.charAt(i);
         }
-        console.log(newStr);
+        console.log(newStr, '--ini apa?');
     }
     testPalindrome(str, start, start + maxLength - 1);
-
+    console.log(tempString, '--line 134')
+    // console.timeEnd('start');
 }
 
-// palindrome('forgeeksskeegfor')
+// palindrome('heeeeys')
 
 
 /**
@@ -149,6 +198,7 @@ const shuffle = (str, l) => {
     let len = str.length;
     //Declare function for all the probability that can make from the array  with 'l' characters
     function probability(str, shuffler, len, l) {
+        // console.time('Start')
         //check the l
         if (l === 0) {
             //Print the shuffle of the array
@@ -158,13 +208,15 @@ const shuffle = (str, l) => {
         // Loop all the characters in the array to add them
         for (let i = 0; i < len; i++) {
             let newShuffler = shuffler + str[i];
+            // console.log(shuffler, '-di loop');
             probability(str, newShuffler, len, l - 1);
         }
+        // console.timeEnd('Start')
     }
     //call the probability function
     (probability(str, "", len, l));
 }
 
 
-let str1 = ['a', 'b', 'c'], l = 3
+let str1 = ['a', 'b'], l = 3
 shuffle(str1, l)
